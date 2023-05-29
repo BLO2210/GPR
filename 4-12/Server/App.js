2541a09ce472b8872a15b332f3ef730cbcec1d40
BLO2210 <brandonmichael2210@gmail.com>
2023-05-29 19:14:01 -0400

    Portfolio Stuff

--- 4-12/Server/App.js
+++ 4-12/Server/App.js
@@ -16,6 +16,7 @@ const authenticate = require('./middlewares/authMiddleware')
 //end of example
 const User = require('./schemas/user')
 const Book = require('./schemas/book')
+const MoviesList = require('./schemas/movieslist')
 const { JsonWebTokenError } = require('jsonwebtoken')
 
 mongoose.connect('mongodb+srv://brandonmichael2210:AoRHZwzwnlbO8elU@cluster0.vjcq3iv.mongodb.net/?retryWrites=true&w=majority')
@@ -26,23 +27,30 @@ mongoose.connect('mongodb+srv://brandonmichael2210:AoRHZwzwnlbO8elU@cluster0.vjc
 })
 
 
-// app.post('/register', async (req, res) => {
-//     const username = req.body.username
-//     const password = req.body.password
+app.post('/register', async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
 
-//     const user = new User({
-//         username: username,
-//         password: password
-//     })
+    const user = new User({
+        username: username,
+        password: password
+    })
+    const salt = await bcrypt.genSalt(10);
 
-//     try {
-//         await user.save()
-//         res.status(200).json({ message: 'Registration successful' })
-//     } catch (error) {
-//         console.error(error)
-//         res.status(500).json({ message: 'Internal server error' })
-//     }
-// })
+    // Hash the password with the salt
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Replace the password with the hashed password
+    user.password = passwordHash;
+
+    try {
+        await user.save()
+        res.status(200).json({ message: 'Registration successful' })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Internal server error' })
+    }
+})
 
 //functional server side login!
 app.post('/api/login', (req, res) => {
@@ -50,18 +58,20 @@ app.post('/api/login', (req, res) => {
 
   User.findOne({ username })
     .then(user => {
+      console.log(user)
       if (!user) {
-        return res.status(401).json({ success: false, message: 'Incorrect username or password' })
+        return res.status(401).json({ success: false, message: 'Incorrect username' })
       }
-
       bcrypt.compare(password, user.password)
         .then(result => {
+          console.log(result)
           if (!result) {
-            return res.status(401).json({ success: false, message: 'Incorrect username or password' })
+            return res.status(401).json({ success: false, message: 'Incorrect password' })
           }
 
           const token = jwt.sign({ username }, 'SECRETKEY')
-          res.json({ success: true, token })
+          //made changes below, added userId
+          res.json({ success: true, token, userId: user._id})
         })
         .catch(err => {
           return res.status(500).json({ success: false, message: 'Internal server error' })
@@ -73,29 +83,130 @@ app.post('/api/login', (req, res) => {
 })
 
 
-app.post('/api/add-book', async (req, res) => {
-    const bookTitle = req.body.bookTitle
-    const bookGenre = req.body.bookGenre
-    const bookPublisher = req.body.bookPublisher
-    const bookYear = req.body.bookYear
-    const bookImageURL = req.body.bookImageURL
+          //potentially delete below
 
-    const book = new Book({
-        bookTitle:bookTitle,
-        bookGenre:bookGenre,
-        bookPublisher:bookPublisher,
-        bookYear:bookYear,
-        bookImageURL:bookImageURL
-    })
-    await book.save()
-    res.json(book)
+// app.post('/:userId/addMovies', async ( req, res) => {
+//   const poster = req.body.movie[0]
+//   const title = req.body.movie[1]
+//   const userId = req.params.userId
+
+//   const addMovieToList = new MoviesList({
+//     poster: poster,
+//     title: title
+//   })
+//   try {
+//     await User.findByIdAndUpdate(userId, {moviesList: addMovieToList})
+//     //trying this
+
+//     // user.moviesList.push(moviesList)
+//     // await user.save()
+//     await addMovieToList.save()
+//     res.status(201).json(addMovieToList)
+//   } catch (err) {
+//     console.error(err)
+//     res.status(500).json({message : 'Server Error'})
+//   }
+  // await moviesList.save()
+  // User.moviesList.push(moviesList)
+  // res.json(moviesList)
+// })
+
+// app.post('/api/add-book', async (req, res) => {
+//     const bookTitle = req.body.bookTitle
+//     const bookGenre = req.body.bookGenre
+//     const bookPublisher = req.body.bookPublisher
+//     const bookYear = req.body.bookYear
+//     const bookImageURL = req.body.bookImageURL
+
+//     const book = new Book({
+//         bookTitle:bookTitle,
+//         bookGenre:bookGenre,
+//         bookPublisher:bookPublisher,
+//         bookYear:bookYear,
+//         bookImageURL:bookImageURL
+//     })
+//     await book.save()
+//     res.json(book)
+// })
+
+app.post('/:userId/addMovies', async (req, res) => {
+  const poster = req.body.movie[0];
+  const title = req.body.movie[1];
+  const userId = req.params.userId;
+
+  try {
+    // Find the current user's document in the User collection
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (!user.moviesList) {
+      // If the user has no movies yet, create a new array with the first movie
+      user.moviesList = [{ poster, title }];
+    } else {
+      // If the user already has movies, add the new movie to the array
+      user.moviesList.push({ poster, title });
+    }
+
+    // Save the updated user document to the database
+    await user.save();
+
+    res.status(201).json(user.moviesList);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+
+app.put(`/users/:userId/movies/:movieId`, async (req, res) => {
+  const {userId, movieId} = req.params
+  const checkboxName = Object.keys(req.body)[0];
+  const checkboxValue = req.body[checkboxName]
+
+  try {
+    const user = await User.findById(userId)
+
+    if (!user) {
+      return res.status(404).json({error: "User not found"})
+    }
+
+    const movie = user.moviesList.find((movie) => movie._id.toString() === movieId)
+
+    if (!movie) {
+      return res.status(404).json({error: "Movie not found"})
+    }
+
+    movie[checkboxName] = checkboxValue
+
+    await user.save()
+
+    res.json(movie)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Server Error")
+  }
 })
 
-app.get('/api/books', async (req, res) => {
-    const books = await Book.find({})
-    res.json(books)
 
+
+app.get('/users/:userId/movies', async (req, res) => {
+  try {
+  const userId = req.params.userId
+  const user = await User.findById(userId)
+  const moviesList = user.moviesList
+  res.json(moviesList)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({message: 'Error retrieving list'})
+  }
 })
+
+// app.get('http://www.omdbapi.com/?apikey=[9efdd8db]&', async (req, res) => {
+//   const movies = await 
+// })
 
 
 app.listen(8080, () => {
